8f0f2aa10fcbb4f9016d9a02b266b411a73397a8
abadrianmr <4Milrod@gmail.com>
2022-11-08 10:15:38 +0100

    Navigation Service

---
+++ WpfUtilities/WpfUtilities/Navigation/Abstract/INavigationService.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace WpfUtilities.Navigation.Abstract
+{
+    public interface INavigationService
+    {
+        public T Show<T, U>() where T : Window where U : INotifyPropertyChanged;
+        public T Show<T>() where T : Window;
+    }
+}
---
+++ WpfUtilities/WpfUtilities/Navigation/Concrete/NavigationService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using Unity;
+using WpfUtilities.Navigation.Abstract;
+
+namespace WpfUtilities.Navigation.Concrete
+{
+    /// <summary>
+    /// Service that manage window navigations
+    /// </summary>
+    public sealed class NavigationService : INavigationService
+    {
+        [Dependency]
+        public IUnityContainer Container { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <returns></returns>
+        public T Show<T, U>() where T : Window where U : INotifyPropertyChanged
+        {
+            var window = Container.Resolve<T>();
+            var viewModel = Container.Resolve<U>();
+            window.DataContext = viewModel;
+            window.Show();
+            return window;
+        }
+
+        public T Show<T>() where T : Window
+        {
+            var window = Container.Resolve<T>();
+
+            string? @namespace = typeof( T ).Namespace?.Replace( ".View", ".ViewModel" );
+            var viewModelName = window.GetType().Name.Replace( "Window", "" ) + "ViewModel";
+            var viewModelType = Type.GetType( string.Format( "{0}.{1}", @namespace, viewModelName ) );
+
+            if( viewModelType == null )
+            {
+                throw new ArgumentException( $"The viewmodel of the view {window.Name} could not be found" );
+            }
+
+            var viewModel = Container.Resolve( viewModelType );
+            window.DataContext = viewModel;
+            window.Show();
+            return window;
+        }
+    }
+}
